b203d0a104bef31661b3ce53b779f8af7bcd0e83
YoungDawnGe <geyangchengyc@163.com>
2021-06-23 13:50:24 +0800

    增加 gin框架

--- main.go
+++ main.go
@@ -1,13 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"gyc.com/Fund/cmd"
+	"regexp"
 )
 
 func main() {
 	//cmd.DownLoadFundTxt("008087")
 	//cmd.GenFundJson("006113")
-	_, data := cmd.JsonToDataArray("006113")
-	fmt.Println(data)
+	//_, data := cmd.JsonToDataArray("006113")
+	//fmt.Println(data)
+
+	r := gin.Default()
+	r.GET("/fund/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/fund/data/:code", func(c *gin.Context) {
+		code := c.Param("code")
+		isCode, _ := regexp.MatchString("\\b\\d{6}\\b", code)
+		if !isCode {
+			c.JSON(405, gin.H{
+				"code": 405,
+				"msg":  "输入的code有误",
+			})
+			return
+		}
+		_, data := cmd.JsonToDataArray(code)
+		c.JSON(200, gin.H{
+			"code": 200,
+			"data": data,
+		})
+	})
+
+	r.GET("/fund/update/:code", func(c *gin.Context) {
+		code := c.Param("code")
+		isCode, _ := regexp.MatchString("\\b\\d{6}\\b", code)
+		if !isCode {
+			c.JSON(405, gin.H{
+				"code": 405,
+				"msg":  "输入的code有误",
+			})
+			return
+		}
+		cmd.DownLoadFundTxt(code)
+		cmd.GenFundJson(code)
+		_, data := cmd.JsonToDataArray(code)
+		c.JSON(200, gin.H{
+			"code": 200,
+			"data": data,
+		})
+	})
+
+	r.Run(":8888")
+
+	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
---
+++ test/json_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+func Test01(t *testing.T) {
+	//str := "[[\"20180808\",\"1.0000\"],[\"20180809\",\"0.9999\"]]"
+	//
+	//arrayObj := make([][]string, 2)
+	//
+	//err := json.Unmarshal([]byte(str), &arrayObj)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println(arrayObj)
+
+	matchString, err := regexp.MatchString("\\b\\d{6}\\b", "000230")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println(matchString)
+	}
+	fmt.Println(matchString)
+
+}
